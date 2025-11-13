9798c54e7f6a8ba570decce5b93779af864b7754
Hermen-Nicolau <hermen.nicolau@gmail.com>
2025-11-13 13:01:42 +0000

    added new validate director script

---
+++ foundations/scripts/validate-director-config.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+
+
+iaas=$1
+environment_name=$2
+product=$3
+
+echo "Validating configuration for product $product"
+
+deploy_type="tile"
+if [ "${product}" == "os-conf" ]; then
+  deploy_type="runtime-config"
+fi
+if [ "${product}" == "clamav" ]; then
+  deploy_type="runtime-config"
+fi
+
+vars_files_args=("")
+if [ -f "../${iaas}/${environment_name}/config/defaults/${product}.yml" ]; then
+  vars_files_args+=("--vars-file ../${iaas}/${environment_name}/config/defaults/${product}.yml")
+fi
+
+if [[ "${deploy_type}" == "runtime-config" ]]; then
+  vars_files_args+=("--vars-file ../${iaas}/${INITIAL_FOUNDATION}/versions/${product}.yml")
+fi
+
+if [ -f "../${iaas}/common/${product}.yml" ]; then
+  vars_files_args+=("--vars-file ../${iaas}/common/${product}.yml")
+fi
+
+if [ -f "../${iaas}/${environment_name}/config/vars/${product}.yml" ]; then
+  vars_files_args+=("--vars-file ../${iaas}/${environment_name}/config/vars/${product}.yml")
+fi
+
+if [ -f "../${iaas}/${environment_name}/config/secrets/${product}.yml" ]; then
+  vars_files_args+=("--vars-file ../${iaas}/${environment_name}/config/secrets/${product}.yml")
+fi
+
+if [ "${deploy_type}" == "tile" ]; then
+  bosh int --var-errs-unused ../${iaas}/${environment_name}/config/templates/${product}.yml ${vars_files_args[@]} > /dev/null
+fi
+
+bosh int --var-errs ../${iaas}/${environment_name}/config/templates/${product}.yml ${vars_files_args[@]} > /dev/null
+echo  "Validation script completed."
